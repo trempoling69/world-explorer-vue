1a63caf751f9077fe0d31c493aca6c22728b4cbc
gor-arnaud <arnaud.michoud@gameofroom.fr>
2023-01-10 22:01:29 +0100

    Exchange rate history component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,119 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>{{ title }}</h2>
+    <input type="text" v-model="filter" />
+    <table class="table">
+      <theader>
+        <tr>
+          <th>Name</th>
+          <th>Languages</th>
+          <th>Currencies</th>
+        </tr>
+      </theader>
+      <tbody>
+        <tr v-for="country in filteredCountries" v-bind:key="country.fifa">
+          <td>
+            <button class="button is-ghost" @click="flag = country.flags.png">
+              {{ country.name.common }}
+            </button>
+          </td>
+          <td>
+            <ul>
+              <li v-for="language in country.languages" :key="language">
+                {{ language }}
+              </li>
+            </ul>
+          </td>
+          <td>
+            <ul>
+              <li v-for="(cur, key) in country.currencies" :key="key">
+                <button
+                  class="button is-ghost"
+                  @click="currency = key"
+                >
+                  {{ key }}
+                </button>
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="modal" :class="{ 'is-active': isFlag }">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <p class="image is-4by3">
+          <img :src="flag" alt="" />
+        </p>
+      </div>
+      <button
+        class="modal-close is-large"
+        aria-label="close"
+        @click="flag = ''"
+      ></button>
+    </div>
+    <div class="modal" :class="{ 'is-active': isCurrency }">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <exchange-rate-history :currency="currency"></exchange-rate-history>
+      </div>
+      <button
+        class="modal-close is-large"
+        aria-label="close"
+        @click="currency = ''"
+      ></button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import ExchangeRateHistory from "./components/ExchangeRateHistory.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { ExchangeRateHistory },
+  data() {
+    return {
+      title: "World Explorer",
+      countries: [],
+      filter: "",
+      flag: "",
+      currency: ""
+    };
+  },
+  methods: {
+    fetchCountries: function () {
+      axios
+        .get("https://restcountries.com/v3.1/all")
+        .then((response) => (this.countries = response.data))
+        .catch((error) => console.error(error));
+    },
+  },
+  computed: {
+    sortedCountries: function () {
+      return [...this.countries].sort((c1, c2) =>
+        c1.name.common.localeCompare(c2.name.common)
+      );
+    },
+    filteredCountries: function () {
+      return this.sortedCountries.filter((c) =>
+        c.name.common.toLowerCase().includes(this.filter.toLowerCase())
+      );
+    },
+    isFlag: function () {
+      return this.flag !== "";
+    },
+    isCurrency: function () {
+      return this.currency !== "";
+    }
+  },
+  mounted() {
+    this.fetchCountries();
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bulma/css/bulma.css";
 </style>
---
+++ src/components/ExchangeRateHistory.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="chart-container">
+    <h2>Exchange rate history for {{ currency }} against EUR</h2>
+    <div ref="chart"></div>
+  </div>
+</template>
+<script>
+import { createChart } from "lightweight-charts";
+import axios from "axios";
+
+let chart = null;
+let lineSeries = null;
+
+export default {
+  name: "ExchangeRateHistory",
+  props: {
+    currency: {
+      type: String,
+      default: "EUR",
+    },
+  },
+  computed: {
+    dataUrl: function () {
+      return `https://api.exchangerate.host/timeseries?start_date=2022-12-01&end_date=2023-01-10&base=EUR&symbols=${this.currency}`;
+    },
+  },
+  methods: {
+    createChart: function () {
+      chart = createChart(this.$refs.chart, {
+        width: 400,
+        height: 300,
+      });
+    },
+    refreshChart: function () {
+      axios
+        .get(this.dataUrl)
+        .then((result) => {
+          const data = Object.keys(result.data.rates).map((key) => {
+            return {
+              time: key,
+              value: result.data.rates[key][this.currency],
+            };
+          });
+
+          if (lineSeries) chart.removeSeries(lineSeries);
+
+          lineSeries = chart.addLineSeries();
+          lineSeries.setData(data);
+        })
+        .catch((error) => console.error(error));
+    },
+    removeChart: function() {
+        if (chart) {
+            chart.remove();
+            chart = null;
+        }
+        if (lineSeries)
+            lineSeries = null;
+
+    }
+  },
+  mounted() {
+    this.createChart();
+    this.refreshChart();
+  },
+  watch: {
+    currency: function () {
+      this.refreshChart();
+    },
+  },
+};
+</script>
